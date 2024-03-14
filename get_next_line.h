6de4c97660fefc0ab82c00bc1974e579e45177a4
Aisha Magoury <amagoury@lab1r1s6.42abudhabi.ae>
2024-03-14 17:01:39 +0400

    GNL

---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amagoury <amagoury@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/04 13:37:44 by amagoury          #+#    #+#             */
+/*   Updated: 2024/02/14 15:31:17 by amagoury         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*check_line(char *s);
+char	*del_line(char *s);
+char	*read_buffer(char *str, int fd);
+char	*get_next_line(int fd);
+#endif
